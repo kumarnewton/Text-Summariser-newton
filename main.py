987570896c6a2d9a04ec6e4661790b5061bb632a
singh reetika <reetikasinghchauhan0@gmail.com>
2024-07-20 14:08:38 +0530

    changes

---
+++ main.py
@@ -0,0 +1,10 @@
+# textSummarizer/main.py
+from src.textSummarizernewton.logging import logger
+
+logger.info("Welcome to our custom logging")
+
+def main():
+    logger.info("This is the main function")
+
+if __name__ == "__main__":
+    main()
